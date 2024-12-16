009a11dd7df37f680c10e452f964338cc6577014
jojusuar <jojusuar@espol.edu.ec>
2024-12-16 14:59:17 -0500

    feat: guia 19

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,24 +1,48 @@
-import { Component, signal } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonCard } from '@ionic/angular/standalone';
+import { Component, signal, ViewChild, ElementRef } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonCard, IonCardContent, IonButton, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { addIcons } from 'ionicons';
 import { cloudUploadOutline } from 'ionicons/icons';
+import { TeachablemachineService } from '../services/teachablemachine.service';
+import { PercentPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonFab, IonFabButton, IonIcon, IonCard],
+  imports: [PercentPipe, IonCardContent, IonButton, IonList, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonFab, IonFabButton, IonIcon, IonCard],
 })
 export class Tab1Page {
+  @ViewChild('image', { static: false }) imageElement!: ElementRef<HTMLImageElement>;
 
   imageReady = signal(false)
   imageUrl = signal("")
+  modelLoaded = signal(false);
+  classLabels: string[] = [];
+  predictions: any[] = [];
 
-  constructor() {
+  constructor(private teachablemachine: TeachablemachineService) {
     addIcons({ cloudUploadOutline });
   }
+
+  async ngOnInit() {
+    await this.teachablemachine.loadModel()
+    this.classLabels = this.teachablemachine.getClassLabels()
+    this.modelLoaded.set(true)
+  }
+
+  async predict() {
+    try {
+      const image = this.imageElement.nativeElement;
+      this.predictions = await this.teachablemachine.predict(image);
+    } catch (error) {
+      console.error(error);
+      alert('Error al realizar la predicción.');
+    }
+  }
+
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
 
